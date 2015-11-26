f6ae3f29043e8b25a50c6c4e0276c3faa8a92702
Verity Cannings <veritycannings@veritys-macbook-pro.local>
2015-11-26 11:22:19 +0000

    Initial Commit

---
+++ Geo Telling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geo Telling
+//
+//  Created by Verity Cannings on 26/11/2015.
+//  Copyright © 2015 Verity Cannings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
